90a43bb65d99c6cbd11c5f31907e0184005bb649
Wesley <wesleytuuh@gmail.com>
2021-09-09 16:39:06 +0200

    wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - CS-Minor-Portfolio',
-    title: 'CS-Minor-Portfolio',
+    title: 'CS Minor Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
