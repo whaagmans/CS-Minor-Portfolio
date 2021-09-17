c881749a6b1218194e3260964ebbc98a5f2847d2
Wesley <wesleytuuh@gmail.com>
2021-09-17 14:57:29 +0200

    +ADD: TICT page + TICT pdf for page

---
+++ pages/tasks/tict.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-container>
+		<embed src="/TICT_Quickscan.pdf" width="100%" height="800px" />
+	</v-container>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			windowHeight: null,
+		};
+	},
+};
+</script>
+
+<style>
+</style>
