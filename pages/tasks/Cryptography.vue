8d7b563593fc7ef59cd9a628d795093e93229c4b
Redeamer <wesleytuuh@gmail.com>
2021-10-07 14:52:51 +0200

    +ADD: placeholder Crypto page

--- pages/tasks/Cryptography.vue
+++ pages/tasks/Cryptography.vue
@@ -1,9 +1,24 @@
 <template>
-	<v-container> </v-container>
+	<v-container>
+		<h1 class="text-h1">{{ article.title }}</h1>
+		<br />
+		<article>
+			<nuxt-content :document="article"></nuxt-content>
+		</article>
+	</v-container>
 </template>
 
 <script>
 export default {
+	async asyncData({ $content, $sentry, error }) {
+		const article = await $content('Cryptography')
+			.fetch()
+			.catch((err) => {
+				error({ statusCode: 404, message: 'Article not found!' });
+				$sentry.captureException(err);
+			});
+		return { article };
+	},
 	head() {
 		return {
 			title: 'Cryptography',
