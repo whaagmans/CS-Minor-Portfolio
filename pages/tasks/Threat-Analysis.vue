bd0a0dc9c8d415ab125cf688977cc6d90a6ab312
Redeamer <wesleytuuh@gmail.com>
2021-11-15 14:24:39 +0100

    wip threat analysis page

--- pages/tasks/Threat-Analysis.vue
+++ pages/tasks/Threat-Analysis.vue
@@ -1,9 +1,24 @@
 <template>
-	<div></div>
+	<v-container
+		><h1 class="text-h1">{{ article.title }}</h1>
+		<br />
+		<article>
+			<nuxt-content :document="article"></nuxt-content>
+		</article>
+	</v-container>
 </template>
 
 <script>
 export default {
+	async asyncData({ $content, $sentry, error }) {
+		const article = await $content('ThreatAnalysis')
+			.fetch()
+			.catch((err) => {
+				error({ statusCode: 404, message: 'Article not found!' });
+				$sentry.captureException(err);
+			});
+		return { article };
+	},
 	head() {
 		return {
 			title: 'Threat Analysis',
@@ -13,4 +28,24 @@ export default {
 </script>
 
 <style>
+img {
+	max-width: 800px;
+	width: 100%;
+	height: 100%;
+	border-radius: 8px;
+}
+
+table,
+th,
+td {
+	border: 1px solid;
+}
+
+table {
+	border-collapse: collapse;
+}
+
+td {
+	padding: 5px;
+}
 </style>
