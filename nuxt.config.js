e5924d135b8b6af6d0cf3c97bacf53ff5bb46576
Wesley <wehaagmans@gmail.com>
2025-01-07 19:55:07 +0100

    build: :wrench: disable server-side rendering in Nuxt configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors';
 
 export default {
+  ssr: false,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s | CS Minor Portfolio',
